4e5ffe80c69f0a57b33bbb0cd0db0635be6f983d
oboutarf <osc.boutarfa@gmail.com>
2023-10-03 09:25:26 +0200

    [fixed .gitignore]

--- serverless_api_sht/src/app/api/hook/route.ts
+++ serverless_api_sht/src/app/api/hook/route.ts
@@ -3,5 +3,5 @@ import { NextResponse, NextRequest } from "next/server";
 export default async function GET(req: NextRequest)   {
     console.log(req)
     const body = { message: 'Serverless responded' }
-    return NextResponse.json(body, { status: 200})
+    return NextResponse.json(body, { status: 200 })
 }
