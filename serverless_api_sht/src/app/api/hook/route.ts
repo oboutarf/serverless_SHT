6ba556bd8005e1a91d3588e28eacc0af7a544d40
oboutarf <osc.boutarfa@gmail.com>
2023-10-03 09:22:15 +0200

    [serverless api activated need to place functions]

---
+++ serverless_api_sht/src/app/api/hook/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export default async function GET(req: NextRequest)   {
+    console.log(req)
+    const body = { message: 'Serverless responded' }
+    return NextResponse.json(body, { status: 200})
+}
